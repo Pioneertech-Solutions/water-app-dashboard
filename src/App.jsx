f6e041b3668442fa0401ab087ce92c0243dfea7d
Mauricio Martinez <mtz0mau2002@gmail.com>
2024-05-20 12:06:48 -0600

    add pwa config with vite pwa plugin

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,10 @@ function App() {
       <DarkProvider>
         <Router />
       </DarkProvider>
+
+      <div className="text-[.6rem] select-none fixed bottom-0 left-0 m-1 z-[999999999999] opacity-35">
+        {import.meta.env.VITE_APP_VERSION || "v0.0.0"}
+      </div>
     </AuthProvider>
   );
 }
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,47 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.svg", "favicon.ico", "robots.txt", "apple-touch-icon.png"],
+      manifest: {
+        name: "Panel administrador de agua",
+        short_name: "Water dashboard",
+        theme_color: "#fde047",
+        background_color: "#030712",
+        display: "standalone",
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+      injectRegister: 'auto',
+      devOptions: {
+        enabled: true,
+      },
+      workbox: {
+        skipWaiting: true,
+        clientsClaim: true,
+      },
+    })
+  ],
 })
